a069a0683f453b994f402d15caa8b83f17fa280e
tkubota <tkubota@mx4.canvas.ne.jp>
2024-07-08 00:40:45 +0900

    add command line options

--- redmine_checker_0609.py
+++ redmine_checker_0609.py
@@ -11,60 +11,11 @@ from pathlib  import Path
 from redminelib import Redmine
 
 
-g_user_name       = 'tkubota'
-g_pass            = 'ABCD1234'
-g_url             = 'http://localhost:3000/'
-g_target_projects = ['TestProject']
-g_user_list       = []
-
-
-
-class cUserData:
-    def __init__(self, id, name):
-        self.id         = id
-        self.name       = name
-        return
-
-
-#/*****************************************************************************/
-#/* ユーザー情報の登録                                                        */
-#/*****************************************************************************/
-def append_user(member):
-    global g_user_list
-
-    for user_data in g_user_list:
-        if (user_data.id == member.user.id) and (user_data.name == member.user.name):
-            return
-
-    user_data = cUserData(member.user.id, member.user.name)
-    g_user_list.append(user_data)
-    print("[%d]:%s" % (user_data.id, user_data.name))
-    return
-
-
-
-#/*****************************************************************************/
-#/* コマンドライン引数処理                                                    */
-#/*****************************************************************************/
-def check_command_line_option():
-    global g_user_name
-    global g_pass
-
-    option = ""
-    sys.argv.pop(0)
-    for arg in sys.argv:
-        if (option == "u"):
-            g_user_name = arg
-        elif (option == "p"):
-            g_pass = arg
-        elif (arg == "-u") or (arg == "--user"):
-            option = "u"
-        elif (arg == "-p") or (arg == "--pass"):
-            option = "p"
-        else:
-            print("invalid arg : %s" % arg)
-
-    return
+g_user_name       = 'admin'
+g_pass            = 'admin'
+g_url             = 'http://localhost/redmine'
+g_project_name    = []
+g_target_users    = ['admin']
 
 
 
@@ -77,7 +28,7 @@ def main():
     global g_url
     global g_project_name
 
-    check_command_line_option()
+#   check_command_line_option()
 #   log_settings()
 
     redmine = Redmine(g_url, username=g_user_name, password=g_pass)
@@ -87,15 +38,22 @@ def main():
     print ("----------------------------------------------------------------------------------------------------------------")
     print (redmine.version)
     print (dir(redmine.version))
-    issue = redmine.issue.get(12)
+    issue = redmine.issue.get(1)
     print ("----------------------------------------------------------------------------------------------------------------")
     projects = redmine.project.all()
     print (projects)
     for project in projects:
         print('[' + str(project.id) + ']' + project.name)
+#       member_ship = redmine.project_membership.get(project.id)
+#       print(dir(member_ship))
+#       project_user = member_ship.user
+#       print(dir(project_user))
+#       print(project_user.name)
         member_ships = project.memberships
+#       print(dir(member_ships))
         for member_ship in member_ships:
-            append_user(member_ship)
+#           print(dir(member_ship))
+            print('  ' + member_ship.user.name)
             pass
 
     print ("----------------------------------------------------------------------------------------------------------------")
