d84271f0481ca03412f66d72f7b42b0fc921c189
tkubota <tkubota@mx4.canvas.ne.jp>
2024-07-22 01:06:55 +0900

    add option file feature

--- redmine_checker.py
+++ redmine_checker.py
@@ -12,32 +12,130 @@ from pathlib  import Path
 from redminelib import Redmine
 
 
-g_opt_user_name    = 'tkubota'
-g_opt_pass         = 'ABCD1234'
-g_opt_full_issues  = 0
-g_opt_url          = 'http://localhost:3000/'
+g_opt_user_name       = 'tkubota'
+g_opt_pass            = 'ABCD1234'
+g_opt_api_key         = ""
+g_opt_full_issues     = 0
+g_opt_url             = 'http://localhost:3000/'
+g_opt_list_attrs      = []
+g_opt_list_cfs        = []
+g_opt_out_file        = 'redmine_result_%ymd.xlsx'
+g_opt_target_projects = []
 
-g_target_projects  = ['TestProject', 'TestSubProject']
+
 g_user_list        = []
+g_cf_type_list     = []
 g_issue_list       = []
-g_filter_limit     = 10
+g_time_entry_list  = []
+g_filter_limit     = 20
+
+
+ATTR_NAME_DIC      = {
+                         'id'                : '#',
+                         'project'           : 'プロジェクト',
+                         'tracker'           : 'トラッカー',
+                         'parent'            : '親チケット',
+                         'status'            : 'ステータス',
+                         'subject'           : 'タイトル',
+                         'author'            : '作成者',
+                         'created_on'        : '作成日',
+                         'priority'          : '優先度',
+                         'assigned_to'       : '担当者',
+                         'updated_on'        : '更新日',
+                         'closed_on'         : '完了日',
+                         'due_date'          : '期日',
+                         'done_ratio'        : '進捗率',
+                         'estimated_hours'   : '予定工数',
+                         'total_spent_hours' : '作業時間',
+                     }
+
+
+class cTimeEntryData:
+    def __init__(self, id, created_on, spent_on, hours, user_data):
+        self.id           = id
+        self.created_on   = created_on
+        self.user         = user_data
+        self.spent_on     = spent_on
+        self.hours        = hours
+        self.updated_on   = ""
+        self.activity     = ""
+        self.project_name = ""
+        self.issue_id     = ""
+        return
+
+
+class cDetailData:
+    def __init__(self, detail):
+        self.property   = detail.get('property')
+        self.name       = detail.get('name')
+        self.old_val    = detail.get('old_value')
+        self.new_val    = detail.get('new_value')
+        return
+
+
+class cJournalData:
+    def __init__(self, id, created_on, user_data):
+        self.id         = id
+        self.created_on = created_on
+        self.user       = user_data
+        self.details    = []
+        self.notes      = ""
+        return
 
 
 class cUserData:
     def __init__(self, id, name):
-        self.id         = id
-        self.name       = name
-        self.activities = []
+        self.id           = id
+        self.name         = name
+        self.time_entries = []
         return
 
 NONE_USER = cUserData(0, "-")
 
 
-class cCustomFieled:
+class cCustomFieledData:
     def __init__(self, cf):
         self.id    = cf.id
         self.name  = cf.name
         self.value = cf.value
+        return
+
+    def get_disp_value(self):
+        if (get_custom_fieled_format(self.id) == "user"):
+            if (self.value == None):
+                return NONE_USER.name
+
+            if (self.value == ""):
+                return NONE_USER.name
+
+            return get_user_data_by_id(int(self.value)).name
+        elif (get_custom_fieled_format(self.id) == "enumeration"):
+            if (self.value == None):
+                return ""
+
+            if (self.value == ""):
+                return ""
+
+            dic = get_custom_fieled_dictionary(self.id)
+            return dic[str(self.value)]
+
+        return self.value
+
+
+
+class cCustomFieledType:
+    def __init__(self, cf):
+        self.id         = cf.id
+        self.name       = cf.name
+        self.type       = cf.customized_type
+        self.format     = cf.field_format
+        self.dictionary = {}
+
+        if (self.format == 'enumeration'):
+            for value_label in cf.possible_values:
+                self.dictionary[value_label['value']] = value_label['label']
+
+#           print(self.dictionary)
         return
 
 
@@ -64,6 +162,14 @@ class cIssueData:
         self.time_entries      = []
         self.custom_fields     = []
         return
+
+    def get_disp_attr(self, attribute):
+        if (attribute == 'author') or (attribute == 'assigned_to'):
+            object = getattr(self, attribute)
+            return object.name
+        else:
+            object = getattr(self, attribute)
+            return object
 
 
 #/*****************************************************************************/
@@ -103,8 +209,17 @@ def print_issue_data(issue_data):
     print("  EstimatedHours  : %s" % (issue_data.estimated_hours))
     print("  TotalSpentHours : %s" % (issue_data.total_spent_hours))
     print("  CustomFields    : ")
+
     for cf_data in issue_data.custom_fields:
-        print("[%s][%s]:%s" % (cf_data.id, cf_data.name, cf_data.value))
+        print("[%s][%s]:%s" % (cf_data.id, cf_data.name, cf_data.get_disp_value()))
+
+    for te_data in issue_data.time_entries:
+        print("  Time Entry[%s][%s]:%s hours in %s by %s" % (te_data.id, te_data.created_on, te_data.hours, te_data.spent_on, te_data.user.name))
+
+    for journal_data in issue_data.journals:
+        print("  Update[%s][%s]:%s" % (journal_data.id, journal_data.created_on, journal_data.user.name))
+        for detail_data in journal_data.details:
+            print("    Detail[%s][%s] %s -> %s" % (detail_data.property, detail_data.name, detail_data.old_val, detail_data.new_val))
 
     return
 
@@ -124,7 +239,7 @@ def read_issue_data(issue):
     issue_data.subject           = issue.subject
     issue_data.priority          = issue.priority.name
 #   print("type priority is : " + str(type(issue_data.priority)))
-    issue_data.tracker           = issue.tracker
+    issue_data.tracker           = issue.tracker.name
     issue_data.subject           = issue.subject
     issue_data.status            = issue.status.name
     issue_data.author            = get_user_data(issue.author)
@@ -147,11 +262,89 @@ def read_issue_data(issue):
     for cf in issue.custom_fields:
 #       print(cf)
 #       print(dir(cf))
-        cf_data = cCustomFieled(cf)
+        cf_data = cCustomFieledData(cf)
         issue_data.custom_fields.append(cf_data)
+
+    for journal in issue.journals:
+#       print(journal)
+#       print(dir(journal))
+#       print("[%s][%s]%s" % (journal.id, journal.created_on, journal.user.name))
+        journal_data = cJournalData(journal.id, journal.created_on, get_user_data_by_id(journal.user.id))
+        for detail in journal.details:
+#           print("---detail---")
+#           print(detail)
+#           print(dir(detail))
+#           for key in detail.keys():
+#               print(key)
+            detail_data = cDetailData(detail)
+            journal_data.details.append(detail_data)
+
+        journal_data.notes = journal.notes
+        issue_data.journals.append(journal_data)
+
+    for time_entry in issue.time_entries:
+        te_data = find_time_entry(time_entry)
+        if (te_data != None):
+            issue_data.time_entries.append(te_data)
+        else:
+            print("time entry not found! id = %s" % (time_entry.id))
 
     print_issue_data(issue_data)
     return
+
+
+#/*****************************************************************************/
+#/* カスタムフィールド情報の取得                                              */
+#/*****************************************************************************/
+def get_custom_fieled_type(id):
+    global g_cf_type_list
+
+    for cf_type in g_cf_type_list:
+        if (cf_type.id == id):
+            return cf_type
+
+    return None
+
+
+#/*****************************************************************************/
+#/* カスタムフィールドのフォーマット情報の取得                                */
+#/*****************************************************************************/
+def get_custom_fieled_format(id):
+    global g_cf_type_list
+
+    cf_type = get_custom_fieled_type(id)
+
+    if (cf_type != None):
+        return cf_type.format
+
+    return ""
+
+
+#/*****************************************************************************/
+#/* カスタムフィールドのフォーマット情報の取得                                */
+#/*****************************************************************************/
+def get_custom_fieled_dictionary(id):
+    global g_cf_type_list
+
+    cf_type = get_custom_fieled_type(id)
+
+    if (cf_type != None):
+        return cf_type.dictionary
+
+    return {}
+
+
+#/*****************************************************************************/
+#/* 作業時間情報の検索                                                        */
+#/*****************************************************************************/
+def find_time_entry(te):
+    global g_time_entry_list
+
+    for te_data in g_time_entry_list:
+        if (te_data.id == te.id):
+            return te_data
+
+    return None
 
 
 #/*****************************************************************************/
@@ -159,6 +352,7 @@ def read_issue_data(issue):
 #/*****************************************************************************/
 def get_user_data(user):
     global g_user_list
+    global g_time_entry_list
 
     if (user == None):
         return cUserData(0, "不明なユーザー")
@@ -168,10 +362,76 @@ def get_user_data(user):
             return user_data
 
     user_data = cUserData(user.id, user.name)
+
+    for time_entry in user.time_entries:
+#       print(time_entry)
+#       print(dir(time_entry))
+        te = cTimeEntryData(time_entry.id, time_entry.created_on, time_entry.spent_on, time_entry.hours, time_entry.user)
+        te.project_name = time_entry.project.name
+        te.issue_id     = time_entry.issue.id
+        te.updated_on   = time_entry.updated_on
+        te.activity     = time_entry.activity
+        print("TimeEntry[%s][%s]spent on : %s  %s hours by %s for #%s in %s, activity : %s" % (time_entry.id, time_entry.created_on, time_entry.spent_on, time_entry.hours, time_entry.user.name, time_entry.issue.id, time_entry.project.name, time_entry.activity))
+        user_data.time_entries.append(te)
+        g_time_entry_list.append(te)
+
     g_user_list.append(user_data)
     print("New user! [%d]:%s" % (user_data.id, user_data.name))
     return user_data
 
+
+#/*****************************************************************************/
+#/* ユーザー情報の登録                                                        */
+#/*****************************************************************************/
+def get_user_data_by_id(user_id):
+    global g_user_list
+
+    for user_data in g_user_list:
+        if (user_data.id == user_id):
+            return user_data
+
+    print("missing user ID! [%d]" % (user_id))
+    return cUserData(0, "不明なユーザー")
+
+
+#/*****************************************************************************/
+#/* コマンドライン引数処理                                                    */
+#/*****************************************************************************/
+def read_setting_file(file_path):
+    global g_opt_url
+    global g_opt_api_key
+    global g_opt_target_projects
+    global g_opt_out_file
+    global g_opt_list_attrs
+
+    f = open(file_path, 'r')
+    lines = f.readlines()
+
+    re_opt_url        = re.compile(r"URL\s+: ([^\n]+)")
+    re_opt_api_key    = re.compile(r"API KEY\s+: ([^\n]+)")
+    re_opt_out_file   = re.compile(r"OUT FILE NAME\s+: ([^\n]+)")
+    re_opt_tgt_prj    = re.compile(r"TARGET PROJECT\s+: ([^\n]+)")
+    re_opt_list_att   = re.compile(r"ISSUE LIST ATTR\s+: ([^\n]+)")
+
+    for line in lines:
+#       print ("line:%s" % line)
+        if (result := re_opt_url.match(line)):
+            g_opt_url = result.group(1)
+        elif (result := re_opt_api_key.match(line)):
+#           print("api key : %s" % (result.group(1)))
+            g_opt_api_key = result.group(1)
+        elif (result := re_opt_tgt_prj.match(line)):
+#           print("target project : %s" % (result.group(1)))
+            g_opt_target_projects.append(result.group(1))
+        elif (result := re_opt_out_file.match(line)):
+#           print("output file : %s" % (result.group(1)))
+            g_opt_out_file = result.group(1)
+        elif (result := re_opt_list_att.match(line)):
+#           print("attr : %s" % (result.group(1)))
+            g_opt_list_attrs.append(result.group(1))
+
+    f.close()
+    return
 
 #/*****************************************************************************/
 #/* コマンドライン引数処理                                                    */
@@ -180,18 +440,31 @@ def check_command_line_option():
     global g_opt_user_name
     global g_opt_pass
     global g_opt_full_issues
+    global g_opt_api_key
 
     option = ""
     sys.argv.pop(0)
     for arg in sys.argv:
         if (option == "u"):
             g_opt_user_name = arg
+            option = ""
         elif (option == "p"):
             g_opt_pass = arg
+            option = ""
+        elif (option == "k"):
+            g_opt_api_key = arg
+            option = ""
+        elif (option == "s"):
+            read_setting_file(arg)
+            option = ""
         elif (arg == "-u") or (arg == "--user"):
             option = "u"
         elif (arg == "-p") or (arg == "--pass"):
             option = "p"
+        elif (arg == "-k") or (arg == "--key"):
+            option = "k"
+        elif (arg == "-s") or (arg == "--set_file"):
+            option = "s"
         elif (arg == "-f") or (arg == "--full"):
             g_opt_full_issues = 1
         else:
@@ -205,6 +478,7 @@ def check_command_line_option():
 #/* 処理開始ログ                                                              */
 #/*****************************************************************************/
 def log_start():
+    global g_opt_out_file
     now = datetime.datetime.now()
 
     time_stamp = now.strftime('%Y%m%d_%H%M%S')
@@ -216,6 +490,11 @@ def log_start():
     now = datetime.datetime.now()
     print("処理開始 : " + str(now))
     print ("----------------------------------------------------------------------------------------------------------------")
+
+    yyyymmdd = now.strftime('%Y%m%d')
+    hhmmss   = now.strftime('%H%M%S')
+    g_opt_out_file = g_opt_out_file.replace('%ymd', yyyymmdd)
+    g_opt_out_file = g_opt_out_file.replace('%hms', hhmmss)
     return start_time
 
 
@@ -236,36 +515,129 @@ def log_end(start_time):
 
 
 #/*****************************************************************************/
+#/* ユーザー情報の取得                                                        */
+#/*****************************************************************************/
+def check_project_member_ships(redmine):
+    global g_opt_target_projects
+
+    print("--------------------------------- Check User Datas ---------------------------------")
+    projects = redmine.project.all()
+
+    #/* 対象プロジェクトからユーザー情報を取得 */
+    for project in projects:
+#       print(project.name)
+#       print(dir(project))
+        for target in g_opt_target_projects:
+            if (target == project.name):
+                for member_ship in project.memberships:
+#                   print(dir(member_ship))
+                    if (hasattr(member_ship, 'user')):
+#                       print(dir(member_ship.user))
+                        get_user_data(member_ship.user)
+
+    return
+
+
+#/*****************************************************************************/
+#/* カスタムフィールド情報の取得                                              */
+#/*****************************************************************************/
+def check_custom_fields(redmine):
+    global g_cf_type_list
+
+    print("--------------------------------- Check Custome Fields ---------------------------------")
+    fields = redmine.custom_field.all()
+    for cf in fields:
+#       print(cf)
+#       print(dir(cf))
+#       if (cf.field_format == 'enumeration'):
+#           print(cf.possible_values)
+#       print(cf.customized_type)
+#       print(cf.field_format)
+        cf_type = cCustomFieledType(cf)
+        if (cf_type.type == "issue"):
+            print("Custom Field[%s][%s]:%s" % (cf_type.id, cf_type.name, cf_type.format))
+            g_cf_type_list.append(cf_type)
+
+    return
+
+
+#/*****************************************************************************/
+#/* 結果フォーマット行出力                                                    */
+#/*****************************************************************************/
+def output_issue_list_format_line(ws):
+    global g_opt_list_attrs
+
+    row = 1
+    col = 1
+    for item in g_opt_list_attrs:
+        ws.cell(row, col).value = ATTR_NAME_DIC[item]
+        col += 1
+
+    return
+
+
+#/*****************************************************************************/
+#/* 結果フォーマット行出力                                                    */
+#/*****************************************************************************/
+def output_issue_list_line(ws, row, issue_data):
+    global g_opt_list_attrs
+
+    col = 1
+    for item in g_opt_list_attrs:
+        ws.cell(row, col).value = issue_data.get_disp_attr(item)
+        col += 1
+
+    return
+
+
+#/*****************************************************************************/
+#/* 結果出力                                                                  */
+#/*****************************************************************************/
+def output_all_issues_list(ws):
+    global g_issue_list
+
+    ws.title = "チケット一覧"
+    output_issue_list_format_line(ws)
+    row = 2
+    for issue_data in g_issue_list:
+        output_issue_list_line(ws, row, issue_data)
+        row += 1
+
+    return
+
+
+#/*****************************************************************************/
+#/* 結果出力                                                                  */
+#/*****************************************************************************/
+def output_datas():
+    global g_opt_out_file
+    wb = openpyxl.Workbook()
+    output_all_issues_list(wb.worksheets[0])
+
+    wb.save(g_opt_out_file)
+    return
+
+
+#/*****************************************************************************/
 #/* メイン関数                                                                */
 #/*****************************************************************************/
 def main():
     global g_opt_user_name
     global g_opt_pass
     global g_opt_url
-    global g_target_projects
     global g_opt_full_issues
+    global g_opt_api_key
 
     check_command_line_option()
     start_time = log_start()
 
-    redmine = Redmine(g_opt_url, username=g_opt_user_name, password=g_opt_pass)
-    projects = redmine.project.all()
+    if (g_opt_api_key == ""):
+        redmine = Redmine(g_opt_url, username=g_opt_user_name, password=g_opt_pass)
+    else:
+        redmine = Redmine(g_opt_url, key=g_opt_api_key)
 
-    #/* 対象プロジェクトからユーザー情報を取得 */
-    for project in projects:
-        print(project.name)
-        print(dir(project))
-        for target in g_target_projects:
-            if (target == project.name):
-                for member_ship in project.memberships:
-                    get_user_data(member_ship.user)
-
-#               print(project.issue_custom_fields)
-#               print(len(project.issue_custom_fields))
-#               for cf in project.issue_custom_fields:
-#                   print(dir(cf))
-
-
+    check_project_member_ships(redmine)
+    check_custom_fields(redmine)
 
     if (g_opt_full_issues):
         print("--------------------------------- Full Issue Check ---------------------------------")
@@ -287,6 +659,7 @@ def main():
     else:
         pass
 
+    output_datas()
     log_end(start_time)
     return
 
